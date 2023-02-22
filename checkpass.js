6ad788dab8a1925f4ab7427faf2ec9483f181e7b
Ni-bu-hui-taotuo <126059927+Ni-bu-hui-taotuo@users.noreply.github.com>
2023-02-22 09:46:57 +0100

    Add files via upload

---
+++ checkpass.js
@@ -0,0 +1,22 @@
+String.prototype.hashCode = function() {
+var hash = 0,
+i, chr;
+if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+      chr = this.charCodeAt(i);
+      hash = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+return hash;
+}
+
+function Click(){
+    var Code = document.getElementById("textbox").value;
+    var rawText = Code.toString();
+    
+    console.log(rawText,rawText.hashCode())
+
+    if(rawText.hashCode() == "-2108395305"){
+        window.location = "https://kogollo52.github.io"
+    }
+}
