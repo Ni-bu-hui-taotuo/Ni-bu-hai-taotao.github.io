3be8b6c9bb20dca15b01b2bd895bc095abe2fcc4
Ni-bu-hui-taotuo <126059927+Ni-bu-hui-taotuo@users.noreply.github.com>
2023-02-22 09:50:56 +0100

    Update checkpass.js

--- checkpass.js
+++ checkpass.js
@@ -5,7 +5,7 @@ if (this.length === 0) return hash;
     for (i = 0; i < this.length; i++) {
       chr = this.charCodeAt(i);
       hash = ((hash << 5) - hash) + chr;
-      hash |= 0; // Convert to 32bit integer
+      hash |= 0;
     }
 return hash;
 }
